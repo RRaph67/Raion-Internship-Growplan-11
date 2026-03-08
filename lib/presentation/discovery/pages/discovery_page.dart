4f507c81551b514c89e9853cba69394276b817ba
RRaph67 <sandhykarafasyatrisnatan@gmail.com>
2026-03-08 12:22:31 +0800

    home page kosong

---
+++ lib/presentation/discovery/pages/discovery_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DiscoveryPage extends StatelessWidget {
+  const DiscoveryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text(
+          "Belum ada konten Discovery",
+          style: TextStyle(fontSize: 16, color: Colors.black54),
+        ),
+      ),
+    );
+  }
+}
